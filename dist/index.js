098d8fef896161ea8432ea46c3e114d9787c51a7
novailoveyou <novailoveyoux@gmail.com>
2021-08-19 13:40:46 +0300

    arrays & ruples & unions & enums

--- dist/index.js
+++ dist/index.js
@@ -3,3 +3,29 @@ let id = 5;
 let company = 'Novailoveyou';
 let isPublished = true;
 let x = 'Hello';
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'Hello'];
+let person = [1, 'Nover', true];
+let employee;
+employee = [
+    [1, 'Nover'],
+    [2, 'Brad'],
+    [4, 'Jill']
+];
+let pid = 22;
+pid = '22';
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction2.Left);
