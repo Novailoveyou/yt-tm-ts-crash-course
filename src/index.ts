037d91607bb1cc78612f278b0045e9499a9fe353
novailoveyou <novailoveyoux@gmail.com>
2021-08-19 18:26:40 +0300

    Implement Interface in Class

--- src/index.ts
+++ src/index.ts
@@ -84,8 +84,14 @@ interface MathFunc {
 const add: MathFunc = (x: number, y: number): number => x + y
 const sub: MathFunc = (x: number, y: number): number => x - y
 
+interface PersonInterface {
+  id: number
+  name: string
+  register(): string
+}
+
 // Classes
-class Person {
+class Person implements PersonInterface {
   id: number
   name: string
 
